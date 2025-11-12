7ccf763d98e3a93e5a715b7722c6f4216ff68c6b
Heloisa <poleseheloisa24@gmail.com>
2025-11-12 10:06:02 -0300

    codigos iniciais, viraMes de Poupanca e ContaCorrente

---
+++ Poupanca (1).js
@@ -0,0 +1,34 @@
+import { Conta } from "./Conta.js";
+
+export class Poupanca extends Conta {
+    #rendimento;
+   
+    constructor(cliente, saldo = 0.0, rendimento = 0.0) {
+        super (cliente, saldo);
+        this.rendimento = rendimento;
+    }
+
+    get rendimento() {
+        return this.#rendimento;
+    }
+    set rendimento(valor) {
+        if (valor < 0.0) {
+            this.#rendimento = 0.0;
+        } else {
+            this.#rendimento = valor;
+        }
+    }
+
+    viraMes(taxaRendimento){ //porcentagem do rendimento do saldo, DEVER SER PARAMETRO???
+        if (taxaRendimento >= 0 && taxaRendimento != null) {
+                 this.#rendimento += super.saldo * (taxaRendimento/100);
+        } 
+    }
+    
+    toString() {
+        return (super.toString() +
+            "\nSaldo = R$" + super.saldo.toFixed(2) +
+            "\nRendimento = R$" + this.#rendimento.toFixed(2) );
+    }
+}
+
